14a7ab4d1343ee5625375bee50512d74f459b1db
fjribptra <bagjaputrafajri@gmail.com>
2024-10-02 21:41:12 +0700

    bug fix

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -9,7 +9,7 @@ export default async function NavigationBar() {
   return (
     <nav className="bg-blue-600 text-white py-5 px-7 fixed top-0 right-0 left-0 flex justify-between z-50">
       <Link href={"/"} className="font-bold">
-        Fake Commerce
+        THS Catalog
       </Link>
       <div className="flex gap-5 items-center">
         <Link href={"/products"} className="hidden">Products</Link>
--- src/components/TypeWriterText.tsx
+++ src/components/TypeWriterText.tsx
@@ -5,7 +5,7 @@ import { Typewriter } from "react-simple-typewriter";
 export default function TypeWriterText() {
     return (
         <div className="text-green-600">
-            <Typewriter words={['This is Fake Commerce', 'See all the products']} cursor loop/>
+            <Typewriter words={['Welcome', 'See all the products']} cursor loop/>
         </div>
     )
 }
